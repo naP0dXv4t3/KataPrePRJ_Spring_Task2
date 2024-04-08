59bf1f8246a3d322aa18158e4878e6ea295ff6db
naP0dXv4t3 <smokewithcheese@gmail.com>
2024-04-09 02:59:27 +0300

    Initial commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,29 @@
+package app.model;
+
+import app.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    @Autowired
+    private Timer timer;
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+    public Timer getTimer(){
+        return timer;
+    }
+}
